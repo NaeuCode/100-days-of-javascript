f159c47bb05cdbb73dfe56a5c6e9fcbc48b4ad84
Naeim <119408921+NaeimArt@users.noreply.github.com>
2023-09-29 17:56:39 +0330

    Adding Day #17

---
+++ Day #17 - Breakout Game/README.md
@@ -0,0 +1,9 @@
+# Day #17
+
+### Breakout Game
+In this tutorial ([Open in Youtube](https://youtu.be/Pp2Iga_E4yE)),  I am gonna showing to you how to code a Breakout Game with javascript. we create a project that you can play breakout game with javascript❗️
+
+# Screenshot
+Here we have project screenshot :
+
+![screenshot](ScreenShot.png)
---
+++ Day #17 - Breakout Game/index.js
@@ -0,0 +1,142 @@
+let canvas = document.getElementById("game"),
+  ctx = canvas.getContext("2d"),
+  ballRadius = 9,
+  x = canvas.width / (Math.floor(Math.random() * Math.random() * 10) + 3),
+  y = canvas.height - 40,
+  dx = 2,
+  dy = -2;
+
+let paddleHeight = 12,
+  paddleWidth = 72;
+
+let paddleX = (canvas.width - paddleWidth) / 2;
+
+let rowCount = 5,
+  columnCount = 9,
+  brickWidth = 54,
+  brickHeight = 18,
+  brickPadding = 12,
+  topOffset = 40,
+  leftOffset = 33,
+  score = 0;
+
+let bricks = [];
+for (let c = 0; c < columnCount; c++) {
+  bricks[c] = [];
+  for (let r = 0; r < rowCount; r++) {
+    bricks[c][r] = { x: 0, y: 0, status: 1 };
+  }
+}
+
+document.addEventListener("mousemove", mouseMoveHandler, false);
+
+function mouseMoveHandler(e) {
+  var relativeX = e.clientX - canvas.offsetLeft;
+  if (relativeX > 0 && relativeX < canvas.width) {
+    paddleX = relativeX - paddleWidth / 2;
+  }
+}
+
+function drawPaddle() {
+  ctx.beginPath();
+  ctx.roundRect(
+    paddleX,
+    canvas.height - paddleHeight,
+    paddleWidth,
+    paddleHeight,
+    30
+  );
+  ctx.fillStyle = "#ad886a";
+  ctx.fill();
+  ctx.closePath();
+}
+
+function drawBall() {
+  ctx.beginPath();
+  ctx.arc(x, y, ballRadius, 0, Math.PI * 2);
+  ctx.fillStyle = "#fff";
+  ctx.fill();
+  ctx.closePath();
+}
+
+function drawBricks() {
+  for (let c = 0; c < columnCount; c++) {
+    for (let r = 0; r < rowCount; r++) {
+      if (bricks[c][r].status === 1) {
+        let brickX = c * (brickWidth + brickPadding) + leftOffset;
+        let brickY = r * (brickHeight + brickPadding) + topOffset;
+        bricks[c][r].x = brickX;
+        bricks[c][r].y = brickY;
+        ctx.beginPath();
+        ctx.roundRect(brickX, brickY, brickWidth, brickHeight, 30);
+        ctx.fillStyle = "#ad886e";
+        ctx.fill();
+        ctx.closePath();
+      }
+    }
+  }
+}
+
+function trackScore() {
+  ctx.font = "bold 16px sans-serif";
+  ctx.fillStyle = "#fff";
+  ctx.fillText("Score : " + score, 8, 24);
+}
+
+function hitDetection() {
+  for (let c = 0; c < columnCount; c++) {
+    for (let r = 0; r < rowCount; r++) {
+      let b = bricks[c][r];
+      if (b.status === 1) {
+        if (
+          x > b.x &&
+          x < b.x + brickWidth &&
+          y > b.y &&
+          y < b.y + brickHeight
+        ) {
+          dy = -dy;
+          b.status = 0;
+          score++;
+
+          if (score === rowCount * columnCount) {
+            alert("You WIN !");
+            document.location.reload();
+          }
+        }
+      }
+    }
+  }
+}
+
+function init() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  trackScore();
+  drawBricks();
+  drawBall();
+  drawPaddle();
+  hitDetection();
+
+  if (x + dx > canvas.width - ballRadius || x + dx < ballRadius) {
+    dx = -dx;
+  }
+
+  if (y + dy < ballRadius) {
+    dy = -dy;
+  } else if (y + dy > canvas.height - ballRadius) {
+    if (x > paddleX && x < paddleX + paddleWidth) {
+      dy = -dy;
+    } else {
+      alert("Game Over ! ");
+      document.location.reload();
+    }
+  }
+
+  if (y + dy > canvas.height - ballRadius || y + dy < ballRadius) {
+    dy = -dy;
+  }
+
+  x += dx;
+  y += dy;
+}
+
+setInterval(init, 10);
